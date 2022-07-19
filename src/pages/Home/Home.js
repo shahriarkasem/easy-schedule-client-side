ea15ad4d8d3e2161d47f2829150059e4a0c3606e
SR-Shahen <s.r.shahenda@gmail.com>
2022-07-19 19:22:51 +0600

    added powrful features

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PowerfulFeatures from './components/PowerfulFeatures/PowerfulFeatures';
 import Review from './components/Review/Review';
 
 const Home = () => {
     return (
         <div>
             <Review></Review>
+            <PowerfulFeatures></PowerfulFeatures>
         </div>
     );
 };
---
+++ src/pages/Home/components/PowerfulFeatures/Feature.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Feature = ({ feature }) => {
+    const { name, description, icon } = feature;
+    return (
+        <div class="card w-80 bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10 flex justify-center">
+                <img src={icon} alt="reminder icon" class="rounded-xl w-10" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="text-xl font-medium px-5">{name}</h2>
+                <p className='my-5'>{description}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Feature;
