4d262ed27c2b449c4433ec66a13bca43154ef1f0
Shahriar Kasem <shahriarkasem@gmail.com>
2022-07-19 17:55:19 +0600

    booking section done for now

--- src/pages/Home/components/Booking/Booking.js
+++ src/pages/Home/components/Booking/Booking.js
@@ -7,8 +7,22 @@ const Booking = () => {
 
     return (
         <div>
-            <div>
+            <div className="md:mx-20 lg:mx-36">
                 <h2 className='font-semibold md:text-2xl'>We handle 1000's of bookings for our users every single day</h2>
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 mt-4">
+                    <div>
+                        <h5 className="text-xl md:text-3xl lg:text-5xl font-bold text-lime-800 mt-3">{todayBooking}</h5>
+                        <p className="text-lime-600 font-medium lg:text-xl mt-1 md:mt-2">Bookings today</p>
+                    </div>
+                    <div>
+                        <h5 className="text-xl md:text-3xl lg:text-5xl font-bold text-lime-800 mt-3">{monthBooking}</h5>
+                        <p className="text-lime-600 font-medium lg:text-xl mt-1 md:mt-2">Bookings this month</p>
+                    </div>
+                    <div>
+                        <h5 className="text-xl md:text-3xl lg:text-5xl font-bold text-lime-800 mt-3">{allTimeBooking}</h5>
+                        <p className="text-lime-600 font-medium lg:text-xl mt-1 md:mt-2">Bookings all time</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
