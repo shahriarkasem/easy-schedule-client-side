d15db04df8dc3fc80be31fcbfa8241b145c774d9
abrakib942 <abrakib942@gmail.com>
2022-07-19 20:02:46 +0600

    support

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Booking from "./components/Booking/Booking";
 import Integration from "./components/integration/Integration";
 import Review from "./components/Review/Review";
+import Support from "./components/support/Support";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <h6>Hello from home.</h6>
       <h5>Testing merge</h5>
       <Integration />
+      <Support />
       <Review></Review>
       <Booking></Booking>
     </div>
