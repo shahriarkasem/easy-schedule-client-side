7020c3bce866a446bfa1d67a24a50b4750b34c1c
abrakib942 <abrakib942@gmail.com>
2022-07-19 18:37:32 +0600

    integration section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Integration from "./components/integration/Integration";
-import Review from "./components/Review/Review";
 
 const Home = () => {
   return (
     <div>
       <Integration />
-      <Review></Review>
     </div>
   );
 };
