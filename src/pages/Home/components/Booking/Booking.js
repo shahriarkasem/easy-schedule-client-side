84a1d9cc253c4f7a062e048a699f11532cbba2ca
Shahriar Kasem <shahriarkasem@gmail.com>
2022-07-19 17:39:13 +0600

    working on booking section

--- src/pages/Home/components/Booking/Booking.js
+++ src/pages/Home/components/Booking/Booking.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Booking = () => {
+    const [todayBooking, setTodayBooking] = useState(48805);
+    const [monthBooking, setMonthBooking] = useState(1040870);
+    const [allTimeBooking, setAllTimeBooking] = useState(81166964);
+
     return (
         <div>
-            
+            <div>
+                <h2 className='font-semibold md:text-2xl'>We handle 1000's of bookings for our users every single day</h2>
+            </div>
         </div>
     );
 };
