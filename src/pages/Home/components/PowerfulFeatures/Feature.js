ab4a6562bf7f28018094155b2242cef4e24cd36b
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-07-20 16:56:14 +0600

    ready banner design

--- src/pages/Home/components/PowerfulFeatures/Feature.js
+++ src/pages/Home/components/PowerfulFeatures/Feature.js
@@ -8,7 +8,7 @@ const Feature = ({ feature }) => {
                 <img src={icon} alt="reminder icon" class="rounded-xl w-10" />
             </figure>
             <div class="card-body items-center text-center">
-                <h2 class="text-xl font-medium px-5">{name}</h2>
+                <h2 class="text-lg font-bold px-5">{name}</h2>
                 <p className='my-5'>{description}</p>
             </div>
         </div>
--- src/pages/Home/components/ReadySignup/ReadySignup.js
+++ src/pages/Home/components/ReadySignup/ReadySignup.js
@@ -3,14 +3,14 @@ import ready from '../../../../media/images/feature/ready.png'
 
 const ReadySignup = () => {
     return (
-        <div className='my-10 bg-green-400 grid grid-cols-2 items-center mx-48 rounded'>
+        <div className='mt-44 mb-10 bg-green-400 grid grid-cols-2 items-center lg:mx-52 rounded-lg'>
             <div className=''>
                 <h1 className='font-medium text-2xl'>Ready? Let's go</h1>
-                <p>7-days trial, no credit card required, no strings attached.</p>
+                <p className='py-2'>7-days trial, no credit card required, no strings attached.</p>
                 <button className='btn btn-black'>SignUp</button>
             </div>
             <div>
-                <img src={ready} alt="" />
+                <img className='-mt-32' src={ready} alt="" />
             </div>
         </div>
     );
