9cb7a97d64dfcb38aa5424a36ab77a2cd21b14c4
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-07-20 23:08:35 +0600

    responsive banner

--- src/pages/Home/components/Banner/Banner.js
+++ src/pages/Home/components/Banner/Banner.js
@@ -8,12 +8,12 @@ const Banner = () => {
 
         <div class="hero min-h-screen " >
             <div class="hero-content flex-col lg:flex-row-reverse text-black flex">
-                <div className='flex-auto w-3/5'>
+                <div className='flex-auto w-1/2'>
                     <img style={{ width: "100%" }} src="https://i.ibb.co/q9pnLvw/banner-Pic.jpg" />
                 </div>
-                <div className='flex-auto w-2/5'>
-                    <h1 class="text-5xl font-bold">APPOINTMENT</h1>
-                    <h1 class="text-5xl ">Booking</h1>
+                <div className='flex-auto w-1/2'>
+                    <h1 class="lg:text-5xl font-bold">APPOINTMENT</h1>
+                    <h1 class="lg:text-5xl ">Booking</h1>
                     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button class="btn btn-primary button-orange">Get Started</button>
                 </div>
