31606e3a1539c257296eb29510694ec20949fdec
JabedH <jabedhtusar@gmail.com>
2022-07-22 03:33:59 +0600

    updated carousel section

--- src/pages/Home/components/EasySchedule/EasySchedule.js
+++ src/pages/Home/components/EasySchedule/EasySchedule.js
@@ -1,21 +1,25 @@
 import {
-    faArrowRightArrowLeft,
-    faBoxArchive,
-    faCartFlatbedSuitcase,
-    faCircleRadiation,
-    faCreditCard,
-    faHeadset,
-    faBusinessTime,
-    faShippingFast,
-    faStar,
-    faTentArrowTurnLeft,
-  } from "@fortawesome/free-solid-svg-icons";
-  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-  import React from "react";
-  
-  const EasySchedule = () => {
-    return (
-      <div className=" p-5  lg:px-12 mt-10">
+  faArrowRightArrowLeft,
+  faBoxArchive,
+  faCartFlatbedSuitcase,
+  faCircleRadiation,
+  faCreditCard,
+  faHeadset,
+  faBusinessTime,
+  faShippingFast,
+  faStar,
+  faTentArrowTurnLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+const EasySchedule = () => {
+  return (
+    <div>
+      <div class="flex flex-col w-full border-opacity-50">
+        <div class="divider"></div>
+      </div>
+      <div className=" p-5  lg:px-12 ">
         <div className="flex justify-center border p-2  ">
           <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-10">
             <div className="flex sm:border-0 md:border-0 lg:border-r-2">
@@ -47,7 +51,10 @@ import {
             <div className="flex  sm:border-0 md:border-0 lg:border-r-2">
               <p className="  lg:text-4xl">
                 {" "}
-                <FontAwesomeIcon style={{ color: "#F88148" }} icon={faHeadset} />
+                <FontAwesomeIcon
+                  style={{ color: "#F88148" }}
+                  icon={faHeadset}
+                />
               </p>
               <div className="text-left px-4">
                 <h3 className=" lg:font-bold uppercase">Call us anytime</h3>
@@ -70,8 +77,8 @@ import {
           </div>
         </div>
       </div>
-    );
-  };
-  
-  export default EasySchedule;
-  
+    </div>
+  );
+};
+
+export default EasySchedule;
