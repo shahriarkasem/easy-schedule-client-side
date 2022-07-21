08aebe2910b00c356efd36deba9008ad3e679c13
JabedH <jabedhtusar@gmail.com>
2022-07-21 12:22:29 +0600

    added carousel

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,36 +1,33 @@
-import React from 'react';
-import Banner from './components/Banner/Banner';
-import Navbar from './components/Navbar/Navbar';
-import Review from './components/Review/Review';
+import React from "react";
+import Banner from "./components/Banner/Banner";
+import Navbar from "./components/Navbar/Navbar";
+import Review from "./components/Review/Review";
 import Booking from "./components/Booking/Booking";
 import Carousel from "./components/Carousel/Carousel";
 
 import Integration from "./components/integration/Integration";
-import PowerfulFeatures from './components/PowerfulFeatures/PowerfulFeatures';
-import ReadySignup from './components/ReadySignup/ReadySignup';
+import PowerfulFeatures from "./components/PowerfulFeatures/PowerfulFeatures";
+import ReadySignup from "./components/ReadySignup/ReadySignup";
 import Support from "./components/support/Support";
 import Schedule from "./components/Schedule/Schedule";
 import Meeting from "./components/Meeting/Meeting";
-
-
+import EasySchedule from "./components/EasySchedule/EasySchedule";
 
 const Home = () => {
-    return (
-        <div>
-
-            <Navbar></Navbar>
-            <Banner></Banner>
-            <Carousel />
-            <Integration />
-            <Support />
-            <Schedule></Schedule>
-            <Meeting></Meeting>
-            <Review></Review>
-            <Booking></Booking>
-            <PowerfulFeatures></PowerfulFeatures>
-            <ReadySignup></ReadySignup>
-
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <EasySchedule />
+      <Carousel />
+      <Integration />
+      <Support />
+      <Schedule></Schedule>
+      <Meeting></Meeting>
+      <Review></Review>
+      <Booking></Booking>
+      <PowerfulFeatures></PowerfulFeatures>
+      <ReadySignup></ReadySignup>
+    </div>
+  );
 };
 export default Home;
