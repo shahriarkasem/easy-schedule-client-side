bc01492699c6920dc56c6a9b1210d08639611ef6
Shahriar Kasem <shahriarkasem@gmail.com>
2022-07-25 22:24:05 +0600

    routes added in app js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/login/SignUp";
+import HowItWorks from "./pages/Common/HowItWorks";
+import Feature from "./pages/Home/components/PowerfulFeatures/Feature";
+import Individuals from "./pages/Common/Individuals";
+import Teams from "./pages/Common/Teams";
+import Pricing from "./pages/Common/Pricing";
+import Blog from "./pages/Common/Resources/Blog";
+import Support from "./pages/Common/Resources/Support";
+import WhatsNew from "./pages/Common/Resources/WhatsNew";
 
 function App() {
   useEffect(() => {
@@ -19,6 +27,14 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/howitworks" element={<HowItWorks></HowItWorks>}></Route>
+        <Route path="/feature" element={<Feature></Feature>}></Route>
+        <Route path="/individuals" element={<Individuals></Individuals>}></Route>
+        <Route path="/teams" element={<Teams></Teams>}></Route>
+        <Route path="/pricing" element={<Pricing></Pricing>}></Route>
+        <Route path="/whatsnew" element={<WhatsNew></WhatsNew>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/support" element={<Support></Support>}></Route>
         <Route path="/aboutus" element={<Aboutus></Aboutus>}></Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
