0abf4bc024aa925c4ab77bbd93606d748dfe50aa
arifphero <ahammadarif232@gmail.com>
2022-07-28 20:50:13 +0600

    setup common route

--- src/pages/Common/HowItWorks.js
+++ src/pages/Common/HowItWorks.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../Home/components/Footer/Footer";
 import Navbar from "../Home/components/Navbar/Navbar";
+import card1 from "../../media/images/howItWorks/card1.webp";
 
 const HowItWorks = () => {
   return (
@@ -17,6 +18,21 @@ const HowItWorks = () => {
         </div>
 
         {/* how its work card */}
+        <div class="card lg:card-side mx-8">
+          <figure>
+            {/* "https://placeimg.com/400/400/arch"  bg-base-100 shadow-xl */}
+            <img className="w-1/2" src={card1} alt="Album" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Connect your calendar</h2>
+            <p>
+              First, connect your Google or Microsoft Outlook.com / Office 365
+              calendar to YouCanBook.me. We use this calendar to check your
+              availability. We also add new events to this calendar when someone
+              schedules time with you.
+            </p>
+          </div>
+        </div>
         <div></div>
       </div>
       <Footer></Footer>
