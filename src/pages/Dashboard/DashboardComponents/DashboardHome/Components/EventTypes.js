b28f15e667070a110c1dfa463f4c2ffd4526217e
Shahriar Kasem <shahriarkasem@gmail.com>
2022-07-28 10:41:11 +0600

    filter part added in event types route

--- src/pages/Dashboard/DashboardComponents/DashboardHome/Components/EventTypes.js
+++ src/pages/Dashboard/DashboardComponents/DashboardHome/Components/EventTypes.js
@@ -2,8 +2,23 @@ import React from "react";
 
 const EventTypes = () => {
   return (
-    <div>
+    <div className="my-12">
       <h2 className="text-3xl">Event Types</h2>
+      <div>
+        <div>
+        <div className="input input-sm  flex flex-row">
+        <span className="mr-[-2px]">🔎</span>
+          <input
+            type="text"
+            placeholder="Filter"
+            class="input input-sm w-full max-w-lg focus:outline-none"
+          />
+        </div>
+        </div>
+        <div>
+          
+        </div>
+      </div>
     </div>
   );
 };
