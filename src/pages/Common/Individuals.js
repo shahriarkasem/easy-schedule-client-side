1a3881181ae9ec0f6597123e744451f1759b9545
arifphero <ahammadarif232@gmail.com>
2022-08-03 21:31:49 +0600

    3rd banner complete

--- src/pages/Common/Individuals.js
+++ src/pages/Common/Individuals.js
@@ -1,9 +1,10 @@
 import React from "react";
 import img1 from "../../media/images/Individuals/img1.webp";
+import img2 from "../../media/images/Individuals/img2.webp";
 const Individuals = () => {
   return (
-    <div className="min-h-screen ">
-      {/* top banner */}
+    <div>
+      {/* top banner  className="min-h-screen" */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mx-12  mt-8">
         <div className="mt-10  ml-20">
           <h2 className="text-4xl font-bold pb-6 ">
@@ -23,6 +24,30 @@ const Individuals = () => {
         </div>
         <div>
           <img src={img1} alt="" />
+        </div>
+      </div>
+
+      {/* 2nd banner */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mx-12  my-18 py-12 ">
+        <div>
+          <img src={img2} alt="" />
+        </div>
+        <div className="mt-10  ml-20">
+          <h4 className="text-blue-600 p">THE EASY SCHEDULE BOOKING LINK</h4>
+          <h2 className="text-3xl font-bold py-6 ">
+            Save time with your own personalized booking links
+          </h2>
+          <p className="text-1xl leading-normal  font-mono pb-10 w-full ">
+            Calendly makes it easy to work smarter when you are working solo.
+            Meetings, sessions, and appointments become more efficient ways to
+            achieve success and accomplish goals.
+          </p>
+          <div>
+            <button class="btn button-orange text-white rounded-full">
+              {" "}
+              Get Your Link
+            </button>
+          </div>
         </div>
       </div>
     </div>
