b71655a25d8e5af75ae737188941a18578de4d94
Shahriar Kasem <shahriarkasem@gmail.com>
2022-08-04 14:42:58 +0600

    useUserEvents hook created and few components added

--- src/pages/Dashboard/DashboardComponents/DashboardHome/Components/EventTypes.js
+++ src/pages/Dashboard/DashboardComponents/DashboardHome/Components/EventTypes.js
@@ -2,17 +2,20 @@ import React, { useEffect, useState } from "react";
 import auth from "../../../../../firebase.init";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
+import useUserEvents from "../../../../../hooks/useUserEvents";
 
 const EventTypes = () => {
   const [user] = useAuthState(auth);
   const navigate = useNavigate();
-  const [firstLetter, setFirstLetter] = useState('');
+  const [firstLetter, setFirstLetter] = useState("");
 
   useEffect(() => {
     const userNameFirstLetter = user?.displayName?.charAt(0);
     setFirstLetter(userNameFirstLetter);
-  },[user])
-  
+  }, [user]);
+
+  const { isLoading, error, userEvents, refetch } = useUserEvents();
+  console.log(userEvents);
 
   return (
     <div className="my-12">
@@ -60,26 +63,62 @@ const EventTypes = () => {
           </div>
         </div>
         {/* events details section */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10 mt-5 md:mt-8">
-          <div class="card bg-base-100 shadow-xl border-t-4 border-red-500">
-            <div class="pt-2 pr-5 pl-5 pb-5">
-              <div className="flex justify-end">
-                <button className="button-orange-border-h40">Edit</button>
+        <div>
+          <div>
+            <h2 className="mt-10 font-semibold text-start text-2xl">OneOnOne Event's</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10 mt-5 md:mt-8">
+              <div class="card bg-base-100 shadow-xl border-t-4 border-red-500">
+                <div class="pt-2 pr-5 pl-5 pb-5">
+                  <div className="flex justify-end">
+                    <button className="button-orange-border-h40">Edit</button>
+                  </div>
+                  <div className="mt-2 md:mt-5">
+                    <h4 className="text-2xl">Event Name</h4>
+                    <p className="font-thin text-slate-400">
+                      Event duration, Event Type
+                    </p>
+                    <p className="my-2 md:my-3 text-blue-600 hover:cursor-pointer hover:underline">
+                      View booking page
+                    </p>
+                  </div>
+                  <div className="flex flex-row justify-between items-center">
+                    <p className="text-blue-600 hover:cursor-pointer">
+                      Copy Link
+                    </p>
+                    <button className="border-2 py-1 px-2 rounded-xl border-blue-400 hover:bg-blue-100 text-blue-500">
+                      Share
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className="mt-2 md:mt-5">
-                <h4 className="text-2xl">Event Name</h4>
-                <p className="font-thin text-slate-400">
-                  Event duration, Event Type
-                </p>
-                <p className="my-2 md:my-3 text-blue-600 hover:cursor-pointer hover:underline">
-                  View booking page
-                </p>
-              </div>
-              <div className="flex flex-row justify-between items-center">
-                <p className="text-blue-600 hover:cursor-pointer">Copy Link</p>
-                <button className="border-2 py-1 px-2 rounded-xl border-blue-400 hover:bg-blue-100 text-blue-500">
-                  Share
-                </button>
+            </div>
+          </div>
+          <div>
+            <h2 className="mt-10 font-semibold text-start text-2xl">Group Event's</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10 mt-5 md:mt-8">
+              <div class="card bg-base-100 shadow-xl border-t-4 border-red-500">
+                <div class="pt-2 pr-5 pl-5 pb-5">
+                  <div className="flex justify-end">
+                    <button className="button-orange-border-h40">Edit</button>
+                  </div>
+                  <div className="mt-2 md:mt-5">
+                    <h4 className="text-2xl">Event Name</h4>
+                    <p className="font-thin text-slate-400">
+                      Event duration, Event Type
+                    </p>
+                    <p className="my-2 md:my-3 text-blue-600 hover:cursor-pointer hover:underline">
+                      View booking page
+                    </p>
+                  </div>
+                  <div className="flex flex-row justify-between items-center">
+                    <p className="text-blue-600 hover:cursor-pointer">
+                      Copy Link
+                    </p>
+                    <button className="border-2 py-1 px-2 rounded-xl border-blue-400 hover:bg-blue-100 text-blue-500">
+                      Share
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
---
+++ src/pages/Dashboard/DashboardComponents/DashboardHome/Components/EventTypesComponents/OneOnOneComponents/OneOnOneEvents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OneOnOneEvents = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default OneOnOneEvents;
