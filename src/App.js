1199d51d0855debb87fea5fda727da6dad6196d6
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-08-04 20:36:38 +0600

    account setting

--- src/App.js
+++ src/App.js
@@ -24,11 +24,8 @@ import EventTypes from "./pages/Dashboard/DashboardComponents/DashboardHome/Comp
 // import ScheduledEvents from "./pages/Dashboard/DashboardComponents/DashboardHome/Components/ScheduledEvents";
 import Workflows from "./pages/Dashboard/DashboardComponents/DashboardHome/Components/Workflows";
 import RoutingForms from "./pages/Dashboard/DashboardComponents/DashboardHome/Components/RoutingForms";
-<<<<<<< HEAD
 import AccountSettings from "./pages/Dashboard/DashboardComponents/AccountSettings";
-=======
 import Billing from "./pages/Dashboard/DashboardComponents/billing/Billing";
->>>>>>> 90938624b200a211282e9184cbb102abab5ddc91
 import Upcoming from "./pages/Dashboard/DashboardComponents/DashboardHome/Components/ScheduledEvent/Upcoming/Upcoming";
 import Pending from "./pages/Dashboard/DashboardComponents/DashboardHome/Components/ScheduledEvent/Pending/Pending";
 import Past from "./pages/Dashboard/DashboardComponents/DashboardHome/Components/ScheduledEvent/Past/Past";
@@ -40,7 +37,7 @@ import OneOnOne from "./pages/Dashboard/DashboardComponents/DashboardHome/Compon
 import Group from "./pages/Dashboard/DashboardComponents/DashboardHome/Components/EventTypesComponents/Group";
 import EventTypesName from "./pages/Dashboard/DashboardComponents/DashboardHome/Components/EventTypesComponents/EventTypesName";
 import DateRange from "./pages/Dashboard/DashboardComponents/DashboardHome/Components/ScheduledEvent/DateRange/DateRange";
-import AccountSettings from "./pages/Dashboard/DashboardComponents/AccountSettings";
+
 import AdminManagement from "./pages/Dashboard/DashboardComponents/AdminManagement";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
