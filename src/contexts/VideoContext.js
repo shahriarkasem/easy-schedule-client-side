69acaefb2c9b04a063b5369b6959ab8416bb02b0
abrakib942 <abrakib942@gmail.com>
2022-08-09 16:52:48 +0600

    call user function in context

--- src/contexts/VideoContext.js
+++ src/contexts/VideoContext.js
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
+import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
@@ -16,6 +17,8 @@ const VideoContextProvider = ({ children }) => {
   const [mic, setMic] = useState(true);
 
   const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
 
   useEffect(() => {
     navigator.mediaDevices
@@ -32,10 +35,46 @@ const VideoContextProvider = ({ children }) => {
     });
   }, []);
 
-  const answerCall = () => {};
-  // const callUser =()=>{
+  const answerCall = () => {
+    setCallAccepted(true);
 
-  // }
+    const peer = new Peer({ initiator: false, trickle: false, stream });
+
+    peer.on("signal", (data) => {
+      socket.emit("answercall", { signal: data, to: call.from });
+    });
+
+    peer.on("stream", (currentStream) => {
+      userVideo.current.srcObject = currentStream;
+    });
+
+    peer.signal(call.signal);
+
+    connectionRef.current = peer;
+  };
+  const callUser = (id) => {
+    const peer = new Peer({ initiator: true, trickle: false, stream });
+
+    peer.on("signal", (data) => {
+      socket.emit("calluser", {
+        userToCall: id,
+        signalData: data,
+        from: me,
+        name,
+      });
+    });
+
+    peer.on("stream", (currentStream) => {
+      userVideo.current.srcObject = currentStream;
+    });
+
+    socket.on("callaccepted", (signal) => {
+      setCallAccepted(true);
+
+      peer.signal(signal);
+    });
+    connectionRef.current = peer;
+  };
   // const leaveCall =()=>{
 
   // }
