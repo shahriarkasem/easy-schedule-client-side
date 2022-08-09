e50531ce87ff4b76832dd7f6f8276112ceefe79e
abrakib942 <abrakib942@gmail.com>
2022-08-09 16:55:45 +0600

    work on video context, it's context api

--- src/contexts/VideoContext.js
+++ src/contexts/VideoContext.js
@@ -75,7 +75,34 @@ const VideoContextProvider = ({ children }) => {
     });
     connectionRef.current = peer;
   };
-  // const leaveCall =()=>{
+  const leaveCall = () => {
+    setCallEnded(true);
 
-  // }
+    connectionRef.current.destroy();
+
+    window.location.reload();
+  };
+
+  return (
+    <SocketContext.Provider
+      value={{
+        call,
+        callAccepted,
+        myVideo,
+        userVideo,
+        stream,
+        name,
+        setName,
+        callEnded,
+        me,
+        callUser,
+        leaveCall,
+        answerCall,
+      }}
+    >
+      {children}
+    </SocketContext.Provider>
+  );
 };
+
+export { VideoContextProvider, SocketContext };
