82393dead9e3bae85fc294ff9326da359ac31854
abrakib942 <abrakib942@gmail.com>
2022-08-11 18:12:41 +0600

    sidebar for call

--- src/contexts/VideoContext.js
+++ src/contexts/VideoContext.js
@@ -25,18 +25,24 @@ const VideoContextProvider = ({ children }) => {
       .getUserMedia({ video: camera, audio: mic })
       .then((currentStream) => {
         setStream(currentStream);
+
         myVideo.current.srcObject = currentStream;
       });
 
-    socket.emit("cm", camera, mic);
+    socket.on("me", (id) => setMe(id));
+
+    socket.on("calluser", ({ from, name: callerName, signal }) => {
+      setCall({ isReceivedCall: true, from, name: callerName, signal });
+    });
+
+    // socket.emit("cm", camera, mic);
   }, [camera, mic]);
 
   if (camera === false) {
     stream.getTracks().forEach((track) => {
       if (track.kind === "video") {
-        track.enabled = false;
-
         track.stop();
+        track.enabled = false;
       }
     });
   }
@@ -49,12 +55,6 @@ const VideoContextProvider = ({ children }) => {
       }
     });
   }
-
-  socket.on("calluser", ({ from, name: callerName, signal }) => {
-    setCall({ isReceivedCall: true, from, name: callerName, signal });
-  });
-
-  socket.on("me", (id) => setMe(id));
 
   const answerCall = () => {
     setCallAccepted(true);
--- src/video/SideBar.js
+++ src/video/SideBar.js
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { SocketContext } from "../contexts/VideoContext";
+import auth from "../firebase.init";
 
 const SideBar = ({ children }) => {
+  const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } =
+    useContext(SocketContext);
+
+  const [idToCall, setIdToCall] = useState("");
+
+  // const [user] = useAuthState(auth);
+
   return (
-    <div>
-      SideBar
+    <div className="w-[90%] lg:w-1/2 my-10 mx-auto">
+      <div className="py-[10px] px-[20px] border border-stone-900">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="flex flex-col"
+          noValidate
+          autoComplete="off"
+        >
+          <div className="grid lg:grid-cols-2 md:grid-cols-2">
+            <div className="flex flex-col p-[20px]">
+              <h6 className="text-xl ">Account Info</h6>
+              <input
+                className="input input-bordered input-primary mt-5"
+                type="text"
+                value={name}
+                fullWidth
+                onChange={(e) => setName(e.target.value)}
+              />
+
+              <CopyToClipboard
+                text={me}
+                className="mt-5 button-orange-border-h40"
+              >
+                <button className="">Copy Id</button>
+              </CopyToClipboard>
+            </div>
+            <div className="flex flex-col p-[20px]">
+              <h6 className="text-xl">Make a call</h6>
+
+              <input
+                className="input input-bordered input-primary mt-5"
+                type="text"
+                value={idToCall}
+                onChange={(e) => setIdToCall(e.target.value)}
+              />
+              {callAccepted && !callEnded ? (
+                <button
+                  className="btn btn-secondary mt-[20px]"
+                  fullWidth
+                  onClick={leaveCall}
+                >
+                  Hang Up
+                </button>
+              ) : (
+                <button
+                  className="btn btn-primary mt-[20px]"
+                  fullWidth
+                  onClick={() => callUser(idToCall)}
+                >
+                  call
+                </button>
+              )}
+            </div>
+          </div>
+        </form>
+      </div>
       {children}
     </div>
   );
