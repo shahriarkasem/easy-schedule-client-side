2f69511c08690b9324c9f6a6a1a4208e0123d967
Shahriar Kasem <shahriarkasem@gmail.com>
2022-08-11 18:21:30 +0600

    dark mode implement done in navbar

--- src/pages/Home/components/Navbar/Navbar.js
+++ src/pages/Home/components/Navbar/Navbar.js
@@ -1,43 +1,101 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import auth from "../../../../firebase.init";
+import DarkContext from "../../../DarkMode/DarkContext";
 import "./Navbar.css";
+import menubar from '../../../../media/images/navbar/hamburger-button-computer-icons-marmon-keystone-canada-menu-png-favpng-8zH9EzUPy4hMCeW7EKgLENdU6.jpg';
+
 const Navbar = () => {
+  const { handleDarkMode, toggle, setToggle } = useContext(DarkContext);
   const [user] = useAuthState(auth);
 
   const navItem = (
     <>
       <li>
-        <Link to="/howitworks">How It Works</Link>
+        <Link to="/howitworks">
+          <span
+            className={"" + (toggle === true ? "text-black" : "text-white")}
+          >
+            How It Works
+          </span>
+        </Link>
       </li>
       <li>
-        <Link to="/features">Features</Link>
+        <Link to="/features">
+          <span
+            className={"" + (toggle === true ? "text-black" : "text-white")}
+          >
+            Features
+          </span>
+        </Link>
       </li>
       <li>
-        <Link to="/individuals">Individuals</Link>
+        <Link to="/individuals">
+          <span
+            className={"" + (toggle === true ? "text-black" : "text-white")}
+          >
+            Individuals
+          </span>
+        </Link>
       </li>
       <li>
-        <Link to="/teams">Teams</Link>
+        <Link to="/teams">
+          <span
+            className={"" + (toggle === true ? "text-black" : "text-white")}
+          >
+            Teams
+          </span>
+        </Link>
       </li>
       <li>
-        <Link to="/pricing">Pricing</Link>
+        <Link to="/pricing">
+          <span
+            className={"" + (toggle === true ? "text-black" : "text-white")}
+          >
+            Pricing
+          </span>
+        </Link>
       </li>
       <li>
-        <Link to="/blog">Resources</Link>
+        <Link to="/blog">
+          <span
+            className={"" + (toggle === true ? "text-black" : "text-white")}
+          >
+            Resources
+          </span>
+        </Link>
       </li>
+      {/* dark mode toggle */}
+      <p
+              className={
+                "flex items-center justify-center my-4 block md:hidden" +
+                (toggle === true
+                  ? ""
+                  : "")
+              }
+            >
+              {
+                toggle === true ? 
+                <p className="flex items-center"><input onClick={() => setToggle(false)} type="checkbox" class="toggle" /></p>
+                :
+                <p className="flex items-center"><input onClick={() => setToggle(true)} type="checkbox" class="toggle toggle-accent" checked /></p>
+              }
+            </p>
     </>
   );
 
   return (
     <div className="container mx-auto">
-      <div class="navbar bg-base-100">
+      <div
+        class={"navbar " + (toggle === true ? "bg-base-100" : "bg-slate-800")}
+      >
         <div class="navbar-start">
           <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <label tabindex="0" class={"btn btn-ghost lg:hidden mr-2 md:mr-5" + (toggle === true ? '' : ' bg-accent btn-outline btn rounded')}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
+                class={"h-5 w-5"}
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -49,31 +107,55 @@ const Navbar = () => {
                   d="M4 6h16M4 12h8m-8 6h16"
                 />
               </svg>
+              {/* <img className="h-6 w-8 rounded-lg" src={menubar}></img> */}
             </label>
             <ul
               tabindex="0"
-              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              class={
+                (toggle === true ? 'menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52' : 'menu menu-compact dropdown-content mt-3 p-2 shadow bg-slate-700 rounded-box w-52')
+              }
             >
               {navItem}
             </ul>
           </div>
-          <div class="w-10 rounded-full">
-            <img src="https://i.ibb.co/QnJPG33/easy-Schedule-Icon.jpg" alt="" />
+          <div class={"w-10 rounded-full"}>
+            <img
+              className="rounded-xl"
+              src="https://i.ibb.co/QnJPG33/easy-Schedule-Icon.jpg"
+              alt=""
+            />
           </div>
-          <Link to="/" class="btn btn-ghost normal-case text-xl">
-            Easy Schedule
+          <Link to="/" class={"btn btn-ghost normal-case text-xl"}>
+            <span className={toggle === true ? "text-black" : "text-white"}>
+              Easy Schedule
+            </span>
           </Link>
         </div>
-        <div class="navbar-center hidden lg:flex ">
-          <ul class="menu menu-horizontal p-0">{navItem}</ul>
+        <div class={"navbar-center hidden lg:flex"}>
+          <ul class={"menu menu-horizontal p-0"}>{navItem}</ul>
         </div>
         <div class="navbar-end">
+            <p
+              className={
+                "flex items-center px-2 md:px-4 lg:px-6 hidden md:block" +
+                (toggle === true
+                  ? ""
+                  : "")
+              }
+            >
+              {
+                toggle === true ? 
+                <p className="flex items-center"><input onClick={() => setToggle(false)} type="checkbox" class="toggle" /></p>
+                :
+                <p className="flex items-center"><input onClick={() => setToggle(true)} type="checkbox" class="toggle toggle-accent" checked /></p>
+              }
+            </p>
           {user ? (
             <Link
               to="/dashboard/d-home/event-types"
               className="button-orange-border-h40"
             >
-              My Account
+              <span>My Account</span>
             </Link>
           ) : (
             <Link to="/login" class="button-orange-border-h40">
