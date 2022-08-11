11ecf20e9a59c5dcce0fc94760619d91dc2f0fe4
Shahriar Kasem <shahriarkasem@gmail.com>
2022-08-11 16:38:11 +0600

    DarkContext added in app js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Aboutus from "./pages/Home/components/Aboutus/Aboutus";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Login from "./pages/login/Login";
@@ -44,69 +44,86 @@ import "react-toastify/dist/ReactToastify.css";
 import SingleDateRange from "./pages/Dashboard/DashboardComponents/DashboardHome/Components/ScheduledEvent/DateRange/SingleDateRange";
 import MessengerCustomerChat from "react-messenger-customer-chat";
 import Call from "./video/call/Call";
+import DarkContext from "./pages/DarkMode/DarkContext";
 
 function App() {
   useEffect(() => {
     AOS.init();
   }, []);
+
+  const [toggle, setToggle] = useState(true);
+
+  const handleDarkMode = () => {
+    if (toggle === true) {
+      setToggle(false);
+    }
+    if (toggle === false) {
+      setToggle(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/howitworks" element={<HowItWorks></HowItWorks>}></Route>
-        <Route path="/features" element={<Features></Features>}></Route>
-        <Route
-          path="/individuals"
-          element={<Individuals></Individuals>}
-        ></Route>
-        <Route path="/teams" element={<Teams></Teams>}></Route>
-        <Route path="/pricing" element={<Pricing></Pricing>}></Route>
-        <Route path="/whatsnew" element={<WhatsNew></WhatsNew>}></Route>
-        <Route path="/blog" element={<Blog></Blog>}></Route>
-        <Route path="/support" element={<Support></Support>}></Route>
-        <Route path="/aboutus" element={<Aboutus></Aboutus>}></Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+    <div>
+      <DarkContext.Provider value={{ handleDarkMode, toggle, setToggle }}>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/howitworks" element={<HowItWorks></HowItWorks>}></Route>
+          <Route path="/features" element={<Features></Features>}></Route>
+          <Route
+            path="/individuals"
+            element={<Individuals></Individuals>}
+          ></Route>
+          <Route path="/teams" element={<Teams></Teams>}></Route>
+          <Route path="/pricing" element={<Pricing></Pricing>}></Route>
+          <Route path="/whatsnew" element={<WhatsNew></WhatsNew>}></Route>
+          <Route path="/blog" element={<Blog></Blog>}></Route>
+          <Route path="/support" element={<Support></Support>}></Route>
+          <Route path="/aboutus" element={<Aboutus></Aboutus>}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
 
-        {/* nested */}
-        <Route path="/dashboard" element={<Dashboard />}>
-          <Route path="/dashboard" element={<DashboardHome />}></Route>
+          {/* nested */}
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="/dashboard" element={<DashboardHome />}></Route>
 
-          <Route path="d-home" element={<DashboardHome />}>
-            {/* home nested */}
-            <Route path="event-types" element={<EventTypes />} />
-            <Route path="Scheduled" element={<ScheduledEvents />}>
-              {/* Scheduled Event nested */}
-              <Route path="Scheduled" element={<Upcoming />}></Route>
-              <Route path="upcoming" element={<Upcoming />}></Route>
-              <Route path="pending" element={<Pending />}></Route>
-              <Route path="past" element={<Past />}></Route>
-              <Route path="date" element={<SingleDateRange />}></Route>
+            <Route path="d-home" element={<DashboardHome />}>
+              {/* home nested */}
+              <Route path="event-types" element={<EventTypes />} />
+              <Route path="Scheduled" element={<ScheduledEvents />}>
+                {/* Scheduled Event nested */}
+                <Route path="Scheduled" element={<Upcoming />}></Route>
+                <Route path="upcoming" element={<Upcoming />}></Route>
+                <Route path="pending" element={<Pending />}></Route>
+                <Route path="past" element={<Past />}></Route>
+                <Route path="date" element={<SingleDateRange />}></Route>
+              </Route>
+              <Route path="workflows" element={<Workflows />} />
+              <Route path="routing" element={<RoutingForms />} />
             </Route>
-            <Route path="workflows" element={<Workflows />} />
-            <Route path="routing" element={<RoutingForms />} />
+
+            <Route path="event-type" element={<EventTypesName />}></Route>
+            <Route path="event-type/one-on-one" element={<OneOnOne />}></Route>
+            <Route path="event-type/group" element={<Group />}></Route>
+            <Route path="availability" element={<Availability />}></Route>
+            <Route path="billing" element={<Billing />}></Route>
+            <Route path="integration" element={<Integrations />}></Route>
+            <Route path="apps" element={<Apps />}></Route>
+            <Route path="help" element={<Help />}></Route>
+            <Route path="accountSetting" element={<AccountSettings />}></Route>
+            <Route
+              path="admin-management"
+              element={<AdminManagement />}
+            ></Route>
+            <Route path="integrations" element={<Integrations />}></Route>
           </Route>
-
-          <Route path="event-type" element={<EventTypesName />}></Route>
-          <Route path="event-type/one-on-one" element={<OneOnOne />}></Route>
-          <Route path="event-type/group" element={<Group />}></Route>
-          <Route path="availability" element={<Availability />}></Route>
-          <Route path="billing" element={<Billing />}></Route>
-          <Route path="integration" element={<Integrations />}></Route>
-          <Route path="apps" element={<Apps />}></Route>
-          <Route path="help" element={<Help />}></Route>
-          <Route path="accountSetting" element={<AccountSettings />}></Route>
-          <Route path="admin-management" element={<AdminManagement />}></Route>
-          <Route path="integrations" element={<Integrations />}></Route>
-        </Route>
-        <Route path="/call" element={<Call />} />
-      </Routes>
-      <MessengerCustomerChat
-        pageId={process.env.REACT_APP_PAGE_ID}
-        appId={process.env.REACT_APP_APP_ID}
-      />
-
-      <Footer />
+          <Route path="/call" element={<Call />} />
+        </Routes>
+        <MessengerCustomerChat
+          pageId={process.env.REACT_APP_PAGE_ID}
+          appId={process.env.REACT_APP_APP_ID}
+        />
+        <Footer />
+      </DarkContext.Provider>
       <ToastContainer />
     </div>
   );
