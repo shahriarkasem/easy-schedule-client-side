c4cdc7c4474d823df623cc4f8fd426328c87d795
arifphero <ahammadarif232@gmail.com>
2022-08-21 03:21:14 +0600

    new page created

---
+++ src/pages/Dashboard/DashboardComponents/IntegrationArif/IntegrationArif.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IntegrationArif = () => {
+  return (
+    <div>
+      <h3>hello from IntegrationArif</h3>
+    </div>
+  );
+};
+
+export default IntegrationArif;
--- src/pages/Dashboard/DashboardComponents/Integrations.js
+++ src/pages/Dashboard/DashboardComponents/Integrations.js
@@ -1,4 +1,5 @@
 import React from "react";
+import IntegrationArif from "./IntegrationArif/IntegrationArif";
 
 const Integrations = () => {
   return (
