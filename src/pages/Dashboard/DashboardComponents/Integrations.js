e4ef783635f48c3c96d4508c6f37b6c0b7cc7503
arifphero <ahammadarif232@gmail.com>
2022-08-26 01:09:00 +0600

    text updated

--- src/pages/Dashboard/DashboardComponents/Integrations.js
+++ src/pages/Dashboard/DashboardComponents/Integrations.js
@@ -4,6 +4,7 @@ import ZoomMeeting from "./ZoomMeeting/ZoomMeeting";
 const Integrations = () => {
   return (
     <div className="min-h-screen">
+      <h3>Integrations</h3>
       <ZoomMeeting />
     </div>
   );
