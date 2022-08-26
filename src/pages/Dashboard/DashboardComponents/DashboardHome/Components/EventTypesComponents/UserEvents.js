ca36cafa14585ecf109c9941f85b520fcf18e1b9
abrakib942 <abrakib942@gmail.com>
2022-08-26 13:07:07 +0600

    video edit

--- src/pages/Dashboard/DashboardComponents/DashboardHome/Components/EventTypesComponents/UserEvents.js
+++ src/pages/Dashboard/DashboardComponents/DashboardHome/Components/EventTypesComponents/UserEvents.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const UserEvents = ({ soloEvent, handleEditEvent }) => {
-  const navigate = useNavigate('');
+  const navigate = useNavigate("");
   const {
     _id,
     userEmail,
@@ -18,20 +18,28 @@ const UserEvents = ({ soloEvent, handleEditEvent }) => {
   } = soloEvent;
   // console.log(soloEvent);
 
-  const openInNewTab = url => {
-    window.open(url, '_blank', 'noopener,noreferrer');
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
   };
   const handleCopyLink = () => {
-    navigator.clipboard.writeText(`https://easy-schedule-77cce.web.app/view-booking/${_id}`);
-    toast.success('Link copied successfully!')
-  }
+    navigator.clipboard.writeText(
+      `https://easy-schedule-77cce.web.app/view-booking/${_id}`
+    );
+    toast.success("Link copied successfully!");
+  };
 
   return (
     <div class="card bg-base-100 shadow-xl border-t-4 border-red-500">
       <div class="pt-2 pr-5 pl-5 pb-5">
         <div className="flex justify-end">
           {/* <button className="button-orange-border-h40">Edit</button> */}
-          <label onClick={()=>handleEditEvent(_id)} for="edit-event-modal" class="button-orange-border-h40 cursor-pointer">Edit</label>
+          <label
+            onClick={() => handleEditEvent(_id)}
+            for="edit-event-modal"
+            class="button-orange-border-h40 cursor-pointer"
+          >
+            Edit
+          </label>
         </div>
         <div className="mt-2 md:mt-5">
           <h4 className="text-2xl">{eventName}</h4>
@@ -39,23 +47,29 @@ const UserEvents = ({ soloEvent, handleEditEvent }) => {
             {eventDuration}min, {eventType}
           </p>
           <p className="font-thin text-slate-500">Date: {eventDate}</p>
-          <p className="mt-2 mb-1 text-blue-600 hover:cursor-pointer hover:underline" onClick={() => openInNewTab(`/view-booking/${_id}`)}>
+          <p
+            className="mt-2 mb-1 text-blue-600 hover:cursor-pointer hover:underline"
+            onClick={() => openInNewTab(`/view-booking/${_id}`)}
+          >
             View booking page
           </p>
         </div>
         <div className="flex flex-row justify-between items-center">
           <p className="text-red-400 hover:cursor-pointer">{location}</p>
-         <div>
-         <button className="border-2 py-1 px-2 rounded-xl border-blue-400 hover:bg-blue-100 text-blue-500 mr-2" onClick={() => handleCopyLink()}>
+        </div>
+        <div className="flex justify-end mt-2">
+          <button
+            className="border-2 py-1 px-2 rounded-xl border-blue-400 hover:bg-blue-100 text-blue-500 mr-2"
+            onClick={() => handleCopyLink()}
+          >
             Copy
           </button>
           <Link
-            to="/call"
+            to="/chat"
             className="border-2 py-1 px-2 rounded-xl border-blue-400 hover:bg-blue-100 text-blue-500"
           >
-            Call
+            Contact
           </Link>
-         </div>
         </div>
       </div>
     </div>
--- src/pages/Shared/Chat/Chat.js
+++ src/pages/Shared/Chat/Chat.js
@@ -8,37 +8,36 @@ const Chat = () => {
   const [user] = useAuthState(auth);
   const { isLoading, error, usersData, refetch } = useUserCollection();
 
-  const[data, setData] = useState()
-  useEffect(()=>{
-let values = [];
-// const copyUsersData = [...usersData];
-const reverse = usersData?.reverse()?.forEach(userData => {
-  const same = user?.email === userData?.email;
-  if(!same){
-    values.push(userData);
-  }
-});
-setData(values)
-  },[usersData])
-  
+  const [data, setData] = useState();
+  useEffect(() => {
+    let values = [];
+    // const copyUsersData = [...usersData];
+    const reverse = usersData?.reverse()?.forEach((userData) => {
+      const same = user?.email === userData?.email;
+      if (!same) {
+        values.push(userData);
+      }
+    });
+    setData(values);
+  }, [usersData]);
+
   return (
     <div className="flex justify-center">
-        <div className="min-h-screen flex flex-col justify-center items-center mt-10">
-      {/* <h4>Here is your messages</h4> */}
-      <h4 className="text-xl font-semibold italic my-2">Send messages</h4>
-       {data ? (
-        <div>
-          {data.map((userData) => (
-            <ChatWithUser 
-            key={userData._id} 
-            user={userData}>
-            </ChatWithUser>
-          ))}
-        </div>
-      ) : (
-        <div></div>
-      )}
-    </div>
+      <div className="min-h-screen flex flex-col justify-center items-center mt-10">
+        {/* <h4>Here is your messages</h4> */}
+        <h4 className="text-xl font-semibold italic my-2">
+          Send message or Call
+        </h4>
+        {data ? (
+          <div>
+            {data.map((userData) => (
+              <ChatWithUser key={userData._id} user={userData}></ChatWithUser>
+            ))}
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
     </div>
   );
 };
