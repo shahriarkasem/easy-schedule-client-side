1c108a2d965c57e6895595e32a78a4c0cf68291c
arifphero <ahammadarif232@gmail.com>
2022-08-26 12:08:25 +0600

    json data updated

--- src/pages/Dashboard/DashboardComponents/ZoomMeeting/ZoomSchedules.js
+++ src/pages/Dashboard/DashboardComponents/ZoomMeeting/ZoomSchedules.js
@@ -5,38 +5,6 @@ import ZoomSchedule from "./ZoomSchedule";
 
 const ZoomSchedules = () => {
   const [schedules, setSchedules] = useState([]);
-  const mySchedule = [
-    {
-      id: 1,
-      title: "Morning Meeting",
-      time: "09:30AM - 11:00AM ",
-      host: "John Akbar",
-    },
-    {
-      id: 2,
-      title: "Afternoon Meeting",
-      time: "03:30AM - 05:00pM ",
-      host: "Tony Stark",
-    },
-    {
-      id: 3,
-      title: "Final Meeting",
-      time: "08:30PM - 10:00PM ",
-      host: "Abrar Sohan",
-    },
-    {
-      id: 4,
-      title: "Error Solving",
-      time: "09:30AM - 10:00PM ",
-      host: "Lokey Martin",
-    },
-    {
-      id: 5,
-      title: "Instant Meeting",
-      time: "09:30AM - 06:00PM ",
-      host: "Karim Jahan",
-    },
-  ];
 
   useEffect(() => {
     fetch("/schedule.json")
