12f0d57c1c030e12a5ee4440010280932d31c8dc
arifphero <ahammadarif232@gmail.com>
2022-09-01 13:19:52 +0600

    handle schedule

--- src/pages/Dashboard/DashboardComponents/ZoomMeeting/ZoomCalender.js
+++ src/pages/Dashboard/DashboardComponents/ZoomMeeting/ZoomCalender.js
@@ -1,29 +1,71 @@
 import React from "react";
+import "./ZoomCalender.css";
 import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import { useForm } from "react-hook-form";
 import { format } from "date-fns";
+import axios from "axios";
+import meetingImg from "../../../../media/images/Individuals/meetingImg.gif";
 
 const ZoomCalender = () => {
   const [date, setDate] = useState(new Date());
-  return (
-    <div>
-      <div class="hero min-h-screen bg-teal-100 my-8">
-        <div class="hero-content flex-col lg:flex-row-reverse">
-          <div>
-            <h3 className="text-3xl font-bold text-orange-500">
-              Want to make a Zoom Meeting?
-            </h3>
-            {/* <button class="btn btn-warning text-center">
-              Join Zoom Meeting
-            </button> */}
-          </div>
-          <div>
-            <DayPicker mode="single" selected={date} onSelect={setDate} />
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    /*  axios({
+      method: "POST",
+      headers: {
+        // authorization
+      },
+      url: `http://localhost:5000/event/create/group`,
+      data: data,
+    }) */
+
+    axios.post("http://localhost:5000/addSchedule", data).then((res) => {
+      console.log(res);
+      if (res.data.insertedId) {
+        alert("added successfully");
+        reset();
+      }
+    });
+  };
+  /* 
+  min-h-screen bg-teal-100 my-8
+  
             <p>
               You have selected:{" "}
               <span className="text-orange-500"> {format(date, "PP")}</span>{" "}
-            </p>
+            </p>*/
+  return (
+    <div>
+      <h3 className="text-2xl font-bold text-orange-500">
+        Want to make a Zoom Meeting?
+      </h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 p-10  min-h-screen bg-teal-100 my-8">
+        <div>
+          <img className="" src={meetingImg} alt="" />
+        </div>
+        <div>
+          <div className="add-service mt-12">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                {...register("title", { required: true })}
+                type="text"
+                placeholder="title"
+              />
+              <input
+                {...register("title", { required: true })}
+                type="time"
+                placeholder="title"
+              />
+              <input
+                {...register("host", { required: true })}
+                placeholder="host name"
+              />
+              <input {...register("zoom")} placeholder="zoom link" />
+              <input className="bg-orange-700 text-white" type="submit" />
+            </form>
           </div>
         </div>
       </div>
