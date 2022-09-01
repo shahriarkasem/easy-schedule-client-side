1e5708b90e3c64ed4ef175643a8d324317935795
JabedH <jabedhtusar@gmail.com>
2022-09-02 01:07:49 +0600

    getting data

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import workflowReducer from "./slices/workflowSlice";
 
 const store = configureStore({
   reducer: {
     users: userReducer,
+    workflows: workflowReducer,
   },
 });
 
---
+++ src/redux/slices/workflowSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchWorkflows = createAsyncThunk(
+  "workflows/fetchWorkflows",
+  async () => {
+    const res = await axios.get("http://localhost:5000/workflow");
+    return res.data;
+  }
+);
+
+export const workflowSlice = createSlice({
+  name: "workflows",
+  initialState: {
+    isLoading: false,
+    workflows: [],
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchWorkflows.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchWorkflows.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.workflows = action.payload;
+      state.error = null;
+    });
+    builder.addCase(fetchWorkflows.rejected, (state, action) => {
+      state.isLoading = false;
+      state.workflows = [];
+      state.error = action.error.message;
+    });
+  },
+});
+export default workflowSlice.reducer;
